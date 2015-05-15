f4cdfafac836c35076f45f1aaa5794bbf9346814
vanadium23 <chernoffivan@gmail.com>
2015-05-15 14:38:45 +0300

    Changed Movie class to fit new templates

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,33 +5,25 @@ movies = [
     media.Movie("The Shawshank Redemption",
                 "http://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
                 "http://www.youtube.com/watch?v=6hB3S9bIaco",
-                "http://www.imdb.com/title/tt0111161/",
-                9.3,
+                '''
+                Two imprisoned men bond over a number of years,
+                    finding solace and eventual redemption through acts of common decency.''',
                 1994),
     media.Movie("The Dark Knight",
                 "http://upload.wikimedia.org/wikipedia/en/8/8a/Dark_Knight.jpg",
                 "http://www.youtube.com/watch?v=yQ5U8suTUw0",
-                "http://www.imdb.com/title/tt0468569/",
-                9.0,
+                '''
+                When the menace known as the Joker wreaks havoc and chaos on the people of Gotham,
+                the caped crusader must come to terms with one of the greatest psychological tests
+                of his ability to fight injustice.''',
                 2008),
     media.Movie("Pulp Fiction",
                 "http://upload.wikimedia.org/wikipedia/en/8/82/Pulp_Fiction_cover.jpg",
                 "http://www.youtube.com/watch?v=s7EdQ4FqbhY",
-                "http://www.imdb.com/title/tt0110912/",
-                8.9,
+                '''
+                The lives of two mob hit men, a boxer, a gangster's wife,
+                and a pair of diner bandits intertwine in four tales of violence and redemption.''',
                 1994),
-    media.Movie("Inception",
-                "http://upload.wikimedia.org/wikipedia/en/7/7f/Inception_ver3.jpg",
-                "http://www.youtube.com/watch?v=66TuSJo4dZM",
-                "http://www.imdb.com/title/tt1375666/",
-                8.8,
-                2010),
-    media.Movie("Star Wars: Episode V - The Empire Strikes Back",
-                "http://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg",
-                "http://www.youtube.com/watch?v=JNwNXF9Y6kY",
-                "http://www.imdb.com/title/tt0080684/",
-                8.8,
-                1980),
     ]
 
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,9 +1,8 @@
 class Movie(object):
     """this class contains required attributes for Movie"""
-    def __init__(self, title, poster_image_url, trailer_youtube_url, imdb_url, imdb_score, year):
+    def __init__(self, title, poster_image_url, trailer_youtube_url, description, year):
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-        self.imdb_url = imdb_url
-        self.imdb_score = imdb_score
+        self.description = description
         self.year = year
